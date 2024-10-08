056689f8c68f65f846156ec3099a499164bfb4fb
meganvchai <mvchai@andrew.cmu.edu>
2024-10-08 14:41:39 -0400

    hw5: fixed -0.00 bug & added pointer cursor to button

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -17,22 +17,21 @@ const original = new Roll("Original", "Sugar Milk", "1", rolls.Original.basePric
 
 const cart = new Set([apple, raisin, walnut, original]);
 
-let totalPrice = 0;
-
 function displayPrice(totalPrice) {
   const calculatedTotal = document.querySelector("#calculated-total");
-  calculatedTotal.textContent = "$" + totalPrice.toFixed(2);
+  calculatedTotal.textContent = "$" + Math.max(0, totalPrice).toFixed(2);
 }
 
+let totalPrice = 0;
+
 function displayCartItem(roll) {
+
   const template = document.querySelector(".cart-item");
   const clone = template.content.cloneNode(true);
 
   const rollType = roll.type;
   const glazingType = roll.glazing;
   const pack = roll.size;
-
-  console.log(rollType);
 
   const removeBtn = clone.querySelector(".remove");
 
